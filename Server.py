4d92b94061d3dcf85b1f64eafe2242334dd6ee2e
Orel Yosupov <Orely4242@gmail.com>
2024-02-24 16:51:58 +0200

    Chat: Initial server connection

--- Server.py
+++ Server.py
@@ -1,5 +1,19 @@
+import socket
+
+IP = "127.0.0.1"
+PORT = 8081
 def main():
-    print("wip")
+    try: 
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
+        server.bind((IP,PORT))
+        server.listen(2)
+            
+    except Exception as e:
+        print("The Error is: " + e)
+    finally:
+        server.close()
+        
+    
 
 if __name__ == "__main__":
     main()
