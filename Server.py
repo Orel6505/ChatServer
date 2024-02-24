e40fdc5d419c952b602c7d6c3947ab20fb05f895
Orel Yosupov <Orely4242@gmail.com>
2024-02-24 16:52:29 +0200

    Chat: Using instead of close

--- Server.py
+++ Server.py
@@ -4,14 +4,14 @@ IP = "127.0.0.1"
 PORT = 8081
 def main():
     try: 
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
-        server.bind((IP,PORT))
-        server.listen(2)
-            
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: #TCP
+            server.bind((IP,PORT))
+            server.listen(2)
+            conn, addr = server.accept()
+            with conn:
+                print(f"Connected by {addr}")
     except Exception as e:
         print("The Error is: " + e)
-    finally:
-        server.close()
         
     
 
