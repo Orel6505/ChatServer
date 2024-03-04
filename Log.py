d89e5bfcc6dd9081568411489fbaaa6718f39125
Orel Yosupov <Orely4242@gmail.com>
2024-03-04 19:57:52 +0200

    Chat: Implement Singleton design pattern in Log.py

--- Log.py
+++ Log.py
@@ -2,18 +2,35 @@ import datetime, time, traceback
 from shutil import move
 
 class Log:
+    __instance = None
+    
+    def getInstance(cls):
+        if cls.__instance is None:
+            return None
+        return cls.__instance
+    
     def __init__(self, filename: str, newFileLog: bool=True) -> None:
-        self.filename = filename
-        if newFileLog:
-            try:
-                move(f'{filename}.log',f'{filename}.{int(time.time())}.log')
-            except FileNotFoundError:
-                print("File Not Exists")
-        self.log = open(f'{filename}.log', "a")
-        if not newFileLog:
-            Log.__write(self,"\n---------beginning of log")
-        else:
-            Log.__write(self,"---------beginning of log")
+        instance = self.getInstance()
+        if instance is None: 
+            self.filename = filename
+            header = "---------beginning of log"
+            if newFileLog:
+                moveResult = self.__moveOldLog()
+            else:
+                header = "\n" + header
+                moveResult = f'Opening {filename}.log'
+            self.log = open(f'{filename}.log', "a")                
+            self.__write(header)
+            self.writeInfo(moveResult)
+            Log.__instance = self
+        self = Log.__instance
+    
+    def __moveOldLog(self) -> str:
+        try:
+            move(f'{self.filename}.log',f'{self.filename}.{int(time.time())}.log')
+            return f'Moved {self.filename}.log to {self.filename}.{int(time.time())}.log'
+        except FileNotFoundError:
+            return "File doesn't exist, Creating it..."
 
     def __write(self, Message: str) -> None:
         try:
@@ -22,23 +39,23 @@ class Log:
             raise OSError(f'Can\'t write to {self.filename}')
     
     def __writeLogEntry(self, LogEntry: str, Message: str) -> None:
-        Log.__write(self,f'{LogEntry}: {datetime.datetime.now().replace(microsecond=0)}: {Message}')
+        self.__write(f'{LogEntry}: {datetime.datetime.now().replace(microsecond=0)}: {Message}')
         
     def writeInfo(self, Message: str) -> None:
-        Log.__writeLogEntry(self,"I", Message)
+        self.__writeLogEntry("I", Message)
     
     def writeWarning(self, Message: str) -> None:
-        Log.__writeLogEntry(self,"W", Message)
+        self.__writeLogEntry("W", Message)
         
     def writeError(self, Message: str) -> None:
-        Log.__writeLogEntry(self,"E", Message)
+        self.__writeLogEntry("E", Message)
 
     def writeFatal(self) -> None:
-        Log.__write(self,"---------Fatal Error---------")
-        Log.__writeLogEntry(self,"F", f'{traceback.format_exc().strip()}')
-        Log.__write(self,"---------Fatal Error---------")
+        self.__write("---------Fatal Error---------")
+        self.__writeLogEntry("F", f'{traceback.format_exc().strip()}')
+        self.__write("---------Fatal Error---------")
 
-    def isActive(self):
+    def isActive(self) -> bool:
         return False if self.log.closed else True
         
     def closeLog(self) -> None:
