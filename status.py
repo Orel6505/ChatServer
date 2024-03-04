d74581ac811462a6b93fa7be7df2eea23a02d622
Orel Yosupov <Orely4242@gmail.com>
2024-03-04 19:57:15 +0200

    Chat: Remove status.py No longer Needed

--- status.py
+++
@@ -1,9 +0,0 @@
-class ServerStatus:
-    def __init__(self):
-        self.status = True
-    
-    def GetServerStatus(self):
-        return self.status
-
-    def SetServerStatus(self, boolean: bool):
-        self.status = boolean
