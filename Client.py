0440ce3dcf7b283c10b2dc85b3c0f0d3b4a5d9a9
Orel Yosupov <Orely4242@gmail.com>
2024-03-04 19:58:23 +0200

    Chat: Create classes for Server and Client

--- Client.py
+++ Client.py
@@ -1,39 +1,90 @@
-import socket, Log
+import socket, Log, threading
+
+class Client:
+    def __init__(self, ip: str, port: int, logName: str="Client") -> None:
+        self.ip = ip
+        self.port = port
+        self.status = True
+        self.log: Log.Log = self.__createLog(logName)
+        self.client: socket.socket = self.createClient()
+    
+    ## Manage Status
+    def getStatus(self) -> bool:
+        return self.status
+
+    def setStatus(self, boolean: bool) -> None:
+        self.status = boolean
+    
+    ## Manage Log class
+    def __createLog(self, logName: str) -> Log.Log:
+        return Log.Log(logName, newFileLog=False)
+
+    def __closeLog(self) -> None:
+        self.log.closeLog()
+        self.log = None
+    
+    ## Manage Client
+    def createClient(self) -> socket.socket:
+        try: 
+            client: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(60)
+            self.log.writeInfo(f'Client Created')
+            return client
+        except Exception:
+            self.log.writeFatal()
+            
+    def closeClient(self) -> None:
+        self.setStatus(False)
+        if self.client:
+            self.log.writeInfo("Client is closed")
+            self.client.close()
+        if self.log:
+            self.__closeLog()
+            
+    def connectToServer(self) -> None:
+        self.client.connect((self.ip,self.port))
+        self.log.writeInfo(f'Client Connected to {self.ip}:{self.port}')
+        
+    def sendData(self) -> None:
+        try:
+            while self.getStatus():
+                message = input("")
+                self.client.send(message.encode())
+                self.log.writeInfo(f'Client send {message} to {self.ip}:{self.port}')
+        except ConnectionResetError:
+            self.log.writeWarning("Server Closed Connection")
+
+    def recvData(self) -> None:
+        try:
+            while self.getStatus():
+                message = self.client.recv(1024)
+                if message == "close":
+                        self.log.writeInfo("Server is closing the server")
+                        self.closeServer()
+                else:
+                    self.log.writeInfo("Server: " + message.decode('utf-8'))
+                    print("Server: " + message.decode('utf-8'))
+        except ConnectionResetError:
+            self.log.writeWarning("Server Closed Connection")
 
 IP = "127.0.0.1"
 PORT = 8081
 
 def main():
     try:
-        log = Log.Log("Client", newFileLog=False)
-        client: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.settimeout(60)
-        log.writeInfo(f'Client Created')
-        client.connect((IP,PORT))
-        log.writeInfo(f'Client Connected to {IP}:{PORT}')
-        msg="1"
-        while msg != "-1":
-            msg = input("")
-            client.send(msg.encode())
-            log.writeInfo(f'Client send {msg} to {IP}:{PORT}')
-        #threading.Thread(target=send_data, args=(client)).start()
-        #threading.Thread(target=recv_data, args=(client)).start()
+        client = Client(IP,PORT)
+        log = client.log
+        client.connectToServer()
+        t1 = threading.Thread(target=client.sendData, args=())
+        t2 = threading.Thread(target=client.recvData, args=())
+        t1.start()
+        t2.start()
+        t2.join()
+        t1.join()
     except Exception as e:
         log.writeFatal()
     finally:
-        client.close()
-        if not client:
-            log.closeLog()
-        
-def send_data(client: socket.socket):
-    while True:
-        msg = input("")
-        client.send(msg.encode())
-
-def recv_data(client: socket.socket):
-    while True:
-        msg = client.recv(1024)
-        print("[SERVER] " + msg.decode('utf-8'))
+        client.closeClient()
 
 if __name__ == "__main__":
     main()
--- Server.py
+++ Server.py
@@ -1,105 +1,145 @@
 import socket, Log, threading
-from status import ServerStatus
 
-status = True
+class Server:
+    def __init__(self, ip: str, port: int, logName: str="Server") -> None:
+        self.ip = ip
+        self.port = port
+        self.status = True
+        self.clients = []
+        self.log: Log.Log = self.__createLog(logName)
+        self.server: socket.socket = self.createServer()
+    
+    ## Manage Clients
+    def getCLients(self) -> list:
+        return self.clients
+    
+    def addClient(self, client: tuple) -> None:
+        self.clients.append(client)
+        self.log.writeInfo(f'New Client connected: {client[1]} ')
+        print(f'New Client connected: {client[1]} ')
+    
+    def removeClient(self, client: tuple) -> None:
+        self.clients.remove(client)
+        self.log.writeInfo(f'Client disconnected {client[1]}')
+        print(f'Client disconnected')
+        client[0].close()
+        
+    def clearClients(self):
+        for client in self.clients:
+            self.removeClient(client)
+    
+    ## Manage Status
+    def getStatus(self) -> bool:
+        return self.status
+
+    def setStatus(self, boolean: bool) -> None:
+        self.status = boolean
+    
+    ## Manage Log class
+    def __createLog(self, logName: str) -> Log.Log:
+        return Log.Log(logName, newFileLog=False)
+
+    def __closeLog(self) -> None:
+        self.log.closeLog()
+        self.log = None
+    
+    ## Manage Server
+    def createServer(self) -> socket.socket:
+        try: 
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind((self.ip,self.port))
+            server.listen(10)
+            server.settimeout(10)
+            self.log.writeInfo("Server Created Successfully")
+            return server
+        except Exception:
+            self.log.writeFatal()
+            
+    def closeServer(self) -> None:
+        self.setStatus(False)
+        if self.clients:
+            self.clearClients()
+        if self.server:
+            self.log.writeInfo("Server Closed")
+            self.server.close()
+        if self.log:
+            self.__closeLog()
+        
+    def acceptClient(self) -> None:
+        try:
+            while self.getStatus():
+                try:
+                    client: tuple = self.server.accept()
+                except TimeoutError:
+                    self.log.writeInfo("Server Timeout")
+                    if not self.getStatus():
+                        break
+                    continue
+                except OSError:
+                    self.log.writeWarning("Server Interrupted")
+                    break
+                self.addClient(client)
+                t = threading.Thread(target=Server.receiveData, args=(self,client))
+                t.start()
+            t.join()
+        except Exception:
+            self.log.writeFatal()
+
+    def receiveData(self, client: tuple) -> None:
+        try: 
+            while self.getStatus():
+                conn: socket.socket = client[0]
+                try: 
+                    data = conn.recv(1024)
+                except ConnectionAbortedError:
+                    break
+                if data == b'':
+                    self.removeClient(client)
+                    break
+                else:
+                    print(f'{data}')
+                    self.log.writeInfo(f'Server Received {data} from {client[1]}')
+        except Exception:
+            self.log.writeFatal()
+            
+    def sendMessage(self) -> None:
+        try:
+            while self.getStatus():
+                message = input("")
+                if message == "close":
+                    self.log.writeInfo("Server is closing the server")
+                    self.closeServer()
+                else:
+                    for client in self.clients:
+                        client[0].send(message.encode())
+                        self.log.writeInfo(f'Sent message "{message}" to address: {client[1]} ')
+        except Exception as e:
+            self.log.writeFatal()
+
 IP = "127.0.0.1"
 PORT = 8081
+
 def main():
     try:
-        clients = []
-        log = Log.Log("Server", newFileLog=False)
-        status: ServerStatus = ServerStatus()
-        server = Create_Server(log)
-        if server:
-            t1= threading.Thread(target=Add_Client, args=((server,clients,log, status)))
+        server = Server(IP,PORT)
+        if server.getStatus():
+            t1= threading.Thread(target=server.acceptClient, args=())
             t1.start()
+            server.sendMessage()
             t1.join()
         else:
-            print("server failed")
-        print("server finished")
-        log.writeInfo("Server Closed")
-    except Exception:
-        log.writeFatal()
-    finally:
-        if server:
-            Close_Server(server, clients)
-        log.closeLog()
-        
-def Create_Server(log: Log.Log) -> socket.socket:
-    try: 
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((IP,PORT))
-        server.listen(10)
-        server.settimeout(10)
-        log.writeInfo("Server Created Successfully")
-        return server
-    except Exception as e:
-        log.writeFatal()
-
-def Close_Server(server: socket.socket, clients: list):
-    for client in clients:
-        clients.remove(client)
-        client[0].close()
-    server.close()
-        
-def Add_Client(server: socket.socket, clients: list, log: Log.Log, status: ServerStatus) -> None:
-    try:
-        while status.GetServerStatus():
-            try:
-                conn: tuple = server.accept()
-                clients.append(conn)
-                log.writeInfo(f'New Client connected: {conn[1]} ')
-                t = threading.Thread(target=Receive_Data, args=(server, clients, conn, log, status))
-                t.start()
-                print(f'New Client connected: {conn[1]} ')
-            except TimeoutError:
-                log.writeWarning("Server Timeout")
-                if not status.GetServerStatus():
-                    break
-            except OSError:
-                log.writeWarning("Server Interrupted")
-                break
-        t.join()
+            print("Server failed")
     except KeyboardInterrupt:
         print('Server interrupted')
-    except Exception:
-        log.writeFatal()
-
-def Receive_Data(server: socket.socket, clients: list, client: tuple, log: Log.Log, status: ServerStatus) -> None:
-    try: 
-        while status.GetServerStatus():
-            conn: socket.socket = client[0]
-            try: 
-                data = conn.recv(1024)
-            except ConnectionAbortedError:
-                break
-            if data == b'':
-                log.writeInfo(f'Client disconnected {client[1]}')
-                print(f'Client disconnected')
-                clients.remove(client)
-                conn.close()
-                break
-            elif data == b'close':
-                log.writeInfo("client is closing the server")
-                Close_Server(server, clients)
-                status.SetServerStatus(False)
-                break
-            else:
-                print(f'{data}')
-                log.writeInfo(f'Server Received {data} from {client[1]}')
-    except Exception:
-        log.writeFatal()
-        
-#async def Send_Messages(server: socket.socket, clients: list, log: Log.Log):
-#    try:
-#        while server:
-#            message = input("")
-#            for client in clients:
-#                conn, addr = client
-#                conn.send(message.encode())
-#                log.writeInfo(f'Sent message "{message}" to address: {addr} ')
-#    except Exception as e:
-#        log.writeFatal()
+    except Exception as e:
+        if server.getStatus():
+            server.log.writeFatal()
+        else:
+            print(e)
+    finally:
+        print("Server Finished")
+        if server.getStatus():
+            server.closeServer()
 
 if __name__ == "__main__":
     main()
